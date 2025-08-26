aa291ba63a57ecccd7db20b36bde34279e648b0a
Zaid <165257574+Uzumaki412@users.noreply.github.com>
2025-08-26 06:54:29 -0400

    Delete Projects/FullStack directory

--- Projects/FullStack/Casestudy/Casestudy/DAL/DomainClasses/OrderSelectionHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Casestudy.DAL.DomainClasses
-{
-    public class OrderSelectionHelper
-    {
-        public int QtyOrdered { get; set; }
-
-        public Product? Product { get; set; }
-    }
-}
